082c9cfe01f9d66ed33d61270e79418cce1c6bfc
Kaladin12 <eliancruz998@gmail.com>
2025-02-15 22:04:09 -0800

    first commit

---
+++ src/main/java/kaladin/zwolf/projects/lastfm/graph/analyzer/LastfmGraphAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package kaladin.zwolf.projects.lastfm.graph.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastfmGraphAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastfmGraphAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kaladin/zwolf/projects/lastfm/graph/analyzer/LastfmGraphAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package kaladin.zwolf.projects.lastfm.graph.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastfmGraphAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
