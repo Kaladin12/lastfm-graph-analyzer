b871f395a724114740fa961f71580a4fa5ab1429
Kaladin12 <eliancruz998@gmail.com>
2025-02-27 00:18:08 -0800

    updated script

--- test.py
+++ test.py
@@ -3,14 +3,12 @@ import re
 
 envVars = dict()
 
+replace_ = lambda x: envVars.get(x.group(1), x.group(0))
+
 with open(".env", "r") as f:
-    data = f.readlines()
-    for i in data:
-        i = i.replace("\n", "")
-        if i != "":
-            pair = i.split("=")
-            envVars[pair[0]] = pair[1]
-            replace_ = lambda x: envVars.get(x.group(1), x.group(0))
-            #print(re.sub(r"\$\{([^}]+)\}", replace_, pair[1]))
-            envVars[pair[0]] = re.sub(r"\$\{([^}]+)\}", replace_, pair[1])
+    f = filter(lambda s: s!="",  map(lambda x: x.replace("\n", ""), f.readlines()))
+    f = map(lambda x: x.split("="), f)
+    for pair in f:
+        envVars[pair[0]] = re.sub(r"\$\{([^}]+)\}", replace_, pair[1])
+
 print(" ".join(f"{key}={envVars[key]}" for key in envVars))
